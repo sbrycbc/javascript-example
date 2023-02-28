21a5263e4b22e034dba515dabab1b937e3806f8a
sbrycbc <sabriyecbc@gmail.com>
2023-02-28 16:00:28 +0100

    begin to learn JS all is ready...

---
+++ first.js
@@ -0,0 +1,16 @@
+/**
+ *  @function nodejs
+ * 
+ */
+
+let a=6;
+console.log(a);
+
+const escapeZeile =`Dies ist\nein mehrzeiliger\nString`;
+console.log(escapeZeile);
+
+const escapeTab = 'Dies ist ein g\te\tt\ta\tb\tb\tt\te\tr string';
+console.log(escapeTab);
+
+
+
