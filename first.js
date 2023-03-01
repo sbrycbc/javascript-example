ca2fe55c13595bc6b49cec877153c57497913380
sbrycbc <sabriyecbc@gmail.com>
2023-03-01 09:28:41 +0100

    Add string examples..

--- first.js
+++ first.js
@@ -12,5 +12,9 @@ console.log(escapeZeile);
 const escapeTab = 'Dies ist ein g\te\tt\ta\tb\tb\tt\te\tr string';
 console.log(escapeTab);
 
+const escapeGans = "Dies ist ein Text mit doppelten \" im String";
+console.log(escapeGans);
 
+const escapeGans2 = 'Dies ist ein Text mit einfachen \' im String';
+console.log(escapeGans2);
 
