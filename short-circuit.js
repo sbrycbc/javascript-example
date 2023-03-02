f15f2fd130c33637816c421694d3669e02b12d57
sbrycbc <sabriyecbc@gmail.com>
2023-03-02 09:57:00 +0100

    added short-circuit

---
+++ short-circuit.js
@@ -0,0 +1,12 @@
+console.log(5 && true);
+console.log(5 && 8);
+console.log(5 || true);
+console.log(5 && true);
+console.log(5 || 8);
+console.log(5 && 0);
+
+console.log(0 && "hallo");
+console.log(false || 555);
+console.log("false" || "gölge");
+
+
