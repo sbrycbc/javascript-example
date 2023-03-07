4b42d7f98dad4fc8bdc617440553b4111e2506ca
sbrycbc <sabriyecbc@gmail.com>
2023-03-07 10:58:38 +0100

    added switch-statements

---
+++ switch-statements.js
@@ -0,0 +1,17 @@
+function divider(input)
+{
+    const spacer = `=`.repeat(20);
+    console.log(`\n` + spacer, input, spacer, `\n`);
+}
+
+divider("SWITCH-STATEMENTS")
+const position = 0;
+switch (position) {
+    case 0:
+        console.log(`Ausgeschaltet`);
+        break;
+
+    case 1:
+        console.log(`Eingeschaltet`);
+        break;
+}
