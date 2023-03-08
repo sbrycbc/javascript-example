014bc3f68d742f96687f2b8638cb28062483db37
sbrycbc <sabriyecbc@gmail.com>
2023-03-08 09:31:39 +0100

    learning if-else...

--- if-ele statement.js
+++ if-ele statement.js
@@ -31,4 +31,55 @@ else
     console.log(`Schlafen!!`);
 }
 
+divider("If / Else Beispiele")
+
+const firstname = `Sabriye`;
+const age = 37;
+const isStudent = false;
+const school = `university`;
+
+if(firstname === `Sabriye`)
+{
+    console.log(`Hello Sabriye`);
+}
+
+if(age === 37)
+{
+    console.log(`you are 37!!`);
+}
+
+if(isStudent === true)
+{
+    console.log(`hi!!`);
+}
+else
+{
+    console.log(`how are you`);
+}
+
+if(age>=18){
+    if((school == `university`) || (school == `high school`)){
+    console.log(`you can drive!!`);}
+    else {
+        console.log(`we have enough education!!!`);
+    }
+}
+
+else
+{
+    console.log(`you can not drive!!!`);
+}
+
+if ( age > 0 && age < 12){
+    console.log(`${firstname} is a child!`);
+}
+else if (age >=13 && age <=19){
+    console.log(`${firstname} is a teenager`);
+}
+else {
+    console.log(`${firstname} is an adult!!`);
+}
+
+
+
 
