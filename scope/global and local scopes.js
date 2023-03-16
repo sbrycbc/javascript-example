cb200dd024a41567813501c8ce076565d79a76dd
sbrycbc <sabriyecbc@gmail.com>
2023-03-16 20:24:11 +0100

    learning scopes methods...

---
+++ scope/global and local scopes.js
@@ -0,0 +1,41 @@
+let name = `Mahir`;
+let age = 25;
+
+function logName(){
+    let name  = `Tarik`;
+    let age = `12`
+    console.log(`function scope`,name,age);
+}
+//console.log(age);
+
+if(true){
+    let age = 30;
+    console.log(`block scope`, name, age);
+}
+
+console.log(age);
+
+logName()
+console.log(name);
+
+
+console.log(`**********************************************`)
+
+if(true){
+    var model = `Opel`;
+    let year = 2016;
+    const color = `white`;
+    console.log(`block scope`, model,year,color);
+}
+
+//console.log(`block scope`, model,year,color);
+console.log(model);
+
+const i = 1;
+
+for(let i=0; i<10;i++){
+    console.log(`i`,i)
+}
+
+console.log(i);
+
