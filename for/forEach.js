662bc7e9d20c92064dd9b98dc46d239746ed4e9b
sbrycbc <sabriyecbc@gmail.com>
2023-04-05 12:17:49 +0200

    learning forEach...

---
+++ for/forEach.js
@@ -0,0 +1,17 @@
+
+let names = [ "Mahir", "Tarik", "Ulas", "Gölge","Sbry",];
+
+names.forEach(function(name){
+   console.log(name);
+});
+
+
+/* for(let i=0; i<names.length; i++){
+    console.log(names[i]);
+} */
+
+/* let i = 0;
+while(i<names.length){
+    console.log(names[i]);
+    i++;
+} */
