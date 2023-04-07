91b7476d763d88de02ed3a485ecd0137ed71284e
sbrycbc <sabriyecbc@gmail.com>
2023-04-07 17:49:12 +0200

    learning callback funtion...

--- callbacks.js
+++ callbacks.js
@@ -41,3 +41,25 @@ console.log();
 console.log("-".repeat(30) + "/".repeat(10) + "-".repeat(30));
 console.log();
 
+
+function a(x,y){
+    console.log(x);
+    y();
+}
+
+function b(){
+    console.log(2);
+}
+
+function c(){
+    console.log(3);
+}
+
+a(5,b);
+a(2,c);
+
+console.log();
+console.log("-".repeat(30) + "/".repeat(10) + "-".repeat(30));
+console.log();
+
+
