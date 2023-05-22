c9a453cb98b25740ec228c8a5db1152f7376304f
sbrycbc <sabriyecbc@gmail.com>
2023-05-22 13:53:58 +0200

    learning promises .then and .catch

--- dom/promies.js
+++ dom/promies.js
@@ -14,3 +14,15 @@ function changeBackgroundColor(color,delay){
 changeBackgroundColor("red", 2000)
 .then(() => changeBackgroundColor("yellow",4000))
 .then(() => changeBackgroundColor("orange",6000))
+
+const smallPromises = new Promise ((resolve, reject) => {
+  if(2>5){
+    resolve("cool");
+  }else{
+    reject("Sorry");
+  }
+});
+
+smallPromises
+.then((val) => console.log(val));
+.catch((message) => console.log(message));
